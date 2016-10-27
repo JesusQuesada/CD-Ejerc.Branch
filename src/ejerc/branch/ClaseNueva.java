f7c613c0d1bdf3fe530660ecd2588e3a811cc5a2
oracle <oracle@atanasoff32.danielcastelao.org>
2016-10-27 08:58:48 +0200

    v.0.2

--- src/ejerc/branch/ClaseNueva.java
+++ src/ejerc/branch/ClaseNueva.java
@@ -4,4 +4,12 @@ package ejerc.branch;
 public class ClaseNueva {
     //atributos
     private float num1,num2;
+    //constructores
+    public void ClaseNueva(){
+        
+    }
+    public void ClaseNueva(float n1, float n2){
+        num1 = n1;
+        num2 = n2;        
+    }
 }
