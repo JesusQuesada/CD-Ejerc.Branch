fd6bc9cfa0c5cf2fff508825ab0e2ba5dd518cb6
oracle <oracle@atanasoff32.danielcastelao.org>
2016-10-27 08:54:51 +0200

    v.0.1

---
+++ src/ejerc/branch/ClaseNueva.java
@@ -0,0 +1,7 @@
+
+package ejerc.branch;
+
+public class ClaseNueva {
+    //atributos
+    private float num1,num2;
+}
---
+++ src/ejerc/branch/EjercBranch.java
@@ -0,0 +1,10 @@
+
+package ejerc.branch;
+
+public class EjercBranch {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
