a773a61754aeb89425f8ebfd81be22f233774d0e
oracle <oracle@atanasoff32.danielcastelao.org>
2016-10-27 09:16:49 +0200

    v.0.4

--- src/ejerc/branch/EjercBranch.java
+++ src/ejerc/branch/EjercBranch.java
@@ -1,10 +1,15 @@
 
 package ejerc.branch;
 
+import javax.swing.JOptionPane;
+
 public class EjercBranch {
 
     public static void main(String[] args) {
-        
+        ClaseNueva cn = new ClaseNueva();
+        float n1 = Float.parseFloat(JOptionPane.showInputDialog("Introducir precio coste "));
+        float n2 = Float.parseFloat(JOptionPane.showInputDialog("Introducir precio venta "));
+        cn.calcularBeneficio(n1, n2);
     }
     
 }
