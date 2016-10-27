c68e5aacbfee843fd9f4c3069207ad1235421647
oracle <oracle@atanasoff32.danielcastelao.org>
2016-10-27 09:08:43 +0200

    exp0.2

--- src/ejerc/branch/ClaseNueva.java
+++ src/ejerc/branch/ClaseNueva.java
@@ -4,5 +4,12 @@ package ejerc.branch;
 public class ClaseNueva {
     //atributos
     private float num1,num2;
-    
+    //métodos
+    public void calcularBeneficio(float num1, float num2){
+        if (num1<num2)
+            System.out.println("El beneficio es el "+(num2-num1)/num1*100+"%");
+        else
+            System.out.println("La perdida es el "+(num1-num2)/num2*100+"%");
+    }
+            
 }
